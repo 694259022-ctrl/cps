ecb5d88b00e7cd2db5432f887e95af2a2acf1c92
694259022-ctrl <69459022@webmail.npru.ac.th>
2026-07-02 16:04:59 +0700

    num.java

---
+++ week3/num.java
@@ -0,0 +1,11 @@
+void main() {
+    IO.print("Enter your number: ");
+    int num1 = Integer.parseInt(IO.readln());
+
+    IO.print("Enter your number: ");
+    int num2 = Integer.parseInt(IO.readln());
+
+    int sum = num1 + num2;
+    
+    IO.println(" Grand total :" + sum );
+    }
