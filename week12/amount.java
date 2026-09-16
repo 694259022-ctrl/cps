e28f2f42bc9c8eaf21188f6e366ea99533bfddef
694259022-ctrl <>
2026-09-16 16:58:39 +0700

    add amount.java

---
+++ week12/amount.java
@@ -0,0 +1,21 @@
+void main() {
+    int balance = 10000;
+    IO.print("Enter your PIN: ");
+    int PIN = Integer.parseInt(IO.readln());
+    if (PIN = 1234 ) {
+        IO.println("===== ATM MENU =====");
+        IO.println("1. Check balance");
+        IO.println("2. Withdraw money");
+        IO.println("3. Deposit money");
+        IO.println("4. Exit");
+
+        IO.println("Select an option: ");
+        int menu = Integer.parseInt(IO.readln());
+        if (menu == 1) {
+            IO.println("Enter the withdrawal amount: ");
+        } 
+
+        
+    }
+}
+    
